7389f7e051d1ed44489fefbf32c2a8de13223a09
Cosmin <CosminDarius1>
2023-07-31 14:11:01 +0100

    Linked the database to PHP page, created an array with the list from database

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$db = new PDO('mysql:host=db;dbname=collectorapp','root','password');
+$db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
+
+$querry = $db->prepare("SELECT `name`,`year`,`director`,`genre` FROM `movies`");
+$querry->execute();
+$data = $querry->fetchAll();
+
+
+
+array_push($data);
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
