e66886043e432931accc27fbe8686227eb902b79
Cosmin <CosminDarius1>
2023-07-31 14:48:25 +0100

    typo mistakes

--- index.php
+++ index.php
@@ -4,11 +4,9 @@ $db = new PDO('mysql:host=db;dbname=collectorapp','root','password');
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
 
-$querry = $db->prepare("SELECT `name`,`year`,`director`,`genre` FROM `movies`");
-$querry->execute();
+$query = $db->prepare("SELECT `name`,`year`,`director`,`genre` FROM `movies`");
+$query->execute();
 $data = $querry->fetchAll();
-
-
 
 echo "<pre>";
 var_dump($data);
