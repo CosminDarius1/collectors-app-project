fc36d29dab10da22453fa70ecab54bc20eaedc09
Cosmin <CosminDarius1>
2023-08-02 16:07:41 +0100

    fixed

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once('../src/Movie.php');
+require_once('src/Movie.php');
 $db = new PDO('mysql:host=db;dbname=collectorapp','root','password');
 $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_ASSOC);
